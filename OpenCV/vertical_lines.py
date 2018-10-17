4561c8b1416fc98c86ef51628225200234adb96a
evansyatich <eyatich@seas.upenn.edu>
2018-10-16 22:00:02 -0400

    i2c

--- OpenCV/vertical_lines.py
+++ OpenCV/vertical_lines.py
@@ -1,13 +1,102 @@
 
-##import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import math
 import os
 import shutil
-import picamera
+import struct
+import smbus
+##import picamera
 import time
+
+def getFloatData(oldFloats):
+    try:
+        data_received = bus.read_i2c_block_data(address, 1, 8)
+        newFloats = [bytes_2_float(data_received, 0)]
+        newFloats.append(bytes_2_float(data_received, 1))
+    except:
+        print("error reading float data")
+        newFloats = oldFloats;
+
+    return newFloats
+
+#
+# 2 = command byte to request second data block from Arduino
+# 4 = number of bytes (one 2-byte word + two bytes)
+#
+def getByteData(oldBytes):
+    try:
+        data_received = bus.read_i2c_block_data(address, 2, 4)
+        newBytes = [data_received[0]*255 + data_received[1]]
+        newBytes.append(data_received[2])
+        newBytes.append(data_received[3])
+    except:
+        print("error reading byte data")
+        newBytes = oldBytes;
+
+    return newBytes
+
+#
+# 255 = command byte to initiate writing to Arduino
+# (arbitrary--must be different than read)
+#
+def putByteList(byteList):
+    try:
+        bus.write_i2c_block_data(address, 255, byteList)
+    except:
+        print("error writing commands")
+    return None
+
+#
+# crazy conversion of groups of 4 bytes in an array into a float
+# simple code assumes floats are at beginning of the array
+# "index" = float index, starting at 0
+#
+def bytes_2_float(data, index):
+    bytes = data[4*index:(index+1)*4]
+    return struct.unpack('f', "".join(map(chr, bytes)))[0]
+
+##########
+# main part of script starts here
+##########
+
+#
+# smbus implements i2c on the RPi
+#
+bus = smbus.SMBus(1)
+
+#
+# this is the Slave address of the Arduino
+#
+address = 0x04
+
+#
+# initialize dummy value of output from Pi (bytes only)
+#
+byteListDummyFromPi = [150, 220]
+
+#
+# initialize dummy values of inputs to Pi
+#
+dummyToPiFloats = [-3.1416, 6.2832]
+dummyToPiBytes = [2047, 50, 50]
+
+#
+# now loop thru reading from and writing to Arduino
+#
+
+
+
+
+
+
+
+
+
+
+####IMAGE PROCESSING#######
 
 
 def grayscale(img):
@@ -44,23 +133,36 @@ def region_of_interest(img, vertices):
 
 def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
 
-    sum_x1 = 0 
-    sum_y1 = 0 
-    sum_x2 = 0
-    sum_y2 = 0
-    count = 0
-
+    gradient = 0
     
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-            sum_x1 += x1
-            sum_y1 += y1
-            sum_x2 += x2
-            sum_y2 += y2
-            count += 1
+             
+            temp = (y1 - y2) / (x1 - x2)
 
-    print (sum_x1 / count, sum_y1 / count, sum_x2 / count, sum_y2 / count)
+            if temp < 0:
+                gradient = (-1 * temp)
+                print(gradient)
+
+    heading = 90 - (math.atan(gradient) * 57.3)
+
+
+    # while True:
+    # # time.sleep(0.1)
+    # # dummyToPiFloats = getFloatData(dummyToPiFloats)
+    # # dummyToPiBytes = getByteData(dummyToPiBytes)
+    # # print(dummyToPiFloats, dummyToPiBytes)
+#
+#   send variable to Pi
+#
+    putByteList([heading, 0])
+
+    print(heading)
+            
+
+    
+    
 
     ## y = mx + b, m = (y1 - y2) / (x1 - x2), b 
 ##return 
@@ -124,11 +226,11 @@ def process_frame(image):
 
 def loop():
     
-    camera = picamera.PiCamera()
-    photoHeight = 540
-    camera.resolution = (16*photoHeight/9, photoHeight)
-    camera.capture('blackRoad.jpg')
-    shutil.move("/home/pi/Autonomous-Vehicle/OpenCV/blackRoad.jpg", "/home/pi/Autonomous-Vehicle/OpenCV/test_images/blackRoad1.jpg")
+    # camera = picamera.PiCamera()
+    # photoHeight = 540
+    # camera.resolution = (16*photoHeight/9, photoHeight)
+    # camera.capture('blackRoad.jpg')
+    # shutil.move("/home/pi/Autonomous-Vehicle/OpenCV/blackRoad.jpg", "/home/pi/Autonomous-Vehicle/OpenCV/test_images/blackRoad1.jpg")
     
     for source_img in os.listdir("test_images/"):
     
